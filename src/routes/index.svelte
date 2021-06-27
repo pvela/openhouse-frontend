fb88ff38bc81a79621512823a3158d325d24f82b
prabhu velayutham <prabhu.v@gmail.com>
2021-06-27 15:26:41 -0600

    added integration to rarible to get nfts for backgrounds

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,11 +5,18 @@
 	import Button from '$lib/components/Button.svelte';
 	import { goto } from '$app/navigation';
 	import { OPENHOUSE_ADDRESS, OPENHOUSE_CONTRACT } from '$lib/contracts/openhouse';
+	import { RARIBLE_ADDRESS, RARIBLE_READ_CONTRACT } from '$lib/contracts/RaribleCollectible.abi';
 	import { writable } from 'svelte/store';
+	import create from 'ipfs-http-client';
+	//const ipfs = create({host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
+
+
+	//import getFromIPFS from '$lib/rarible/yourcollectible.svelte';
 
 	const chain = getContext('chain');
 	const address = getContext('address');
 	const rooms = writable([]);
+	const nfts = writable([]);
 
 	export let conferenceId = '';
 	const onFormSubmit = () => {
@@ -24,6 +31,43 @@
 			$rooms = result;
 		});
 	}
+	let raribleContract;
+	$: if (chain && $chain) {
+		raribleContract = new $chain.eth.Contract(RARIBLE_READ_CONTRACT, RARIBLE_ADDRESS);
+		const balanceMethod = raribleContract.methods.balanceOf($address);
+		balanceMethod.call().then((balance) => {
+			console.log("🤗 balance:", balance);
+			for (let tokenIndex = 0; tokenIndex < balance; tokenIndex++) {
+				try {
+					console.log("Getting token index", tokenIndex);
+					raribleContract.methods.tokenOfOwnerByIndex($address, tokenIndex).call().then((tokenId) => {
+						console.log("tokenId", tokenId);
+
+						const tokenURI = raribleContract.methods.tokenURI(tokenId).call().then((tokenURI) =>{
+							console.log("tokenURI", tokenURI);
+
+							const ipfsHash = tokenURI.replace("https://ipfs.io/ipfs/", "");
+							console.log("ipfsHash", ipfsHash);
+
+							//const jsonManifestBuffer = new getFromIPFS(ipfsHash);
+
+							try {
+								const jsonManifest = {};//JSON.parse(jsonManifestBuffer.toString());
+								console.log("jsonManifest", jsonManifest);
+								$nfts.push({ id: tokenId, uri: tokenURI, owner: address, ...jsonManifest });
+							} catch (e) {
+								console.log(e);
+							}
+						})
+					});
+				} catch (e) {
+					console.log(e);
+				}
+			}
+			console.log($nfts);
+		});
+	}
+
 </script>
 
 {#if $address !== ROOT_ADDRESS}
@@ -45,6 +89,26 @@
 		</div>
 	{:else}
 		<p>There are no conferences registered yet. Use the form above to be the first to register.</p>
+	{/if}
+</section>
+
+<section>
+	<h2>Available NFTs to be used as background</h2>
+	{#if $nfts && $nfts.length}
+			{#each $nfts as item}
+				const id = item.id.toNumber();
+				const key={item.id.toNumber() + "_" + item.uri + "_" + item.owner}
+				<div>
+					<span>#{item.id.toNumber()}</span> {item.name}
+				</div>
+				<div>
+				<img src={item.image} alt={item.description} 
+				 />
+				</div>
+				<div>{item.description}</div>
+			{/each}
+	{:else}
+		<p>There are no NFTs in your account</p>
 	{/if}
 </section>
 
